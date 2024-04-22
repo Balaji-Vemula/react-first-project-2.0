ecc815c8499041dbfe5badfc7fef2697db1464e3
vemula <vemulabalaji1998@gmail.com>
2024-04-22 23:43:36 +0530

    dynamic content using '{varible_name}'

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import reactCoreConcepts from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+function Header() {
+  const starter = reactDescription[getRandomInt(2)]
+  return (
+    <header>
+      <img src={reactCoreConcepts} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {starter} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
