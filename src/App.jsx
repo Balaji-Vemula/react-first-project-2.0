8c9c1243614b73ce279636062535d404a736859a
vemula <vemulabalaji1998@gmail.com>
2024-04-23 13:07:54 +0530

    Core Concepts cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import reactCoreConcepts from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescription = ["Fundamental", "Crucial", "Core"];
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
-  const starter = reactDescription[getRandomInt(2)]
+  const starter = reactDescription[getRandomInt(2)];
   return (
     <header>
       <img src={reactCoreConcepts} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {starter} React concepts you will need for almost any app you are
-        going to build!
+        {starter} React concepts you will need for almost any app you are going
+        to build!
       </p>
     </header>
+  );
+}
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -23,7 +33,41 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {/* <CoreConcept
+              title="Components"
+              description="The core UI building block - compose the user interface by combining multiple components."
+              image={componentsImg}
+            />
+            <CoreConcept
+              title="JSX"
+              description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered."
+              image={jsxImg}
+            />
+            <CoreConcept
+              title="Props"
+              description="Make components configurable (and therefore reusable) by passing input data to them."
+              image={propsImg}
+            />
+            <CoreConcept
+              title="State"
+              description="React-managed data which, when changed, causes the component to re-render & the UI to update."
+              image={stateImg}
+            /> */}
+
+            {CORE_CONCEPTS.map((item) => {
+              return (
+                <CoreConcept
+                  title={item.title}
+                  description={item.description}
+                  image={item.image}
+                />
+              );
+            })}
+          </ul>
+        </section>
       </main>
     </div>
   );
